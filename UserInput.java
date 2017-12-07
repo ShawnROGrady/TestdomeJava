5e9d38bf08b0cdab5dceedb1d980d73ba937d858
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-07 17:22:49 -0600

    Added problem statement

--- UserInput.java
+++ UserInput.java
@@ -1,3 +1,17 @@
+//  Created by Shawn O'Grady on 12/7/17.
+//  Copyright © 2017 Shawn O'Grady. All rights reserved.
+//
+/*
+ This code is a practice Java interview question from testdome.com
+ 
+ Problem statement: User interface contains two types of user input controls: TextInput, which accepts all characters and NumericInput, which accepts only digits.
+	-Implement the class TextInput that contains:
+		i) Public method void add(char c) - adds the given character to the current value
+		ii) Public method String getValue() - returns the current value
+	-Implement the class NumericInput that:
+		i) Inherits from TextInput
+		ii) Overrides the add method so that each non-numeric character is ignored
+ */
 package userInput;
 
 public class UserInput {
