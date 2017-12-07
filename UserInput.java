b8d4bd8b070e119e005a991d4761997d912d4478
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-07 17:17:50 -0600

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,16 @@
+package userInput;
+
+public class UserInput {
+    
+    public static class TextInput {}
+
+    public static class NumericInput {}
+
+    public static void main(String[] args) {
+        //TextInput input = new NumericInput();
+        //input.add('1');
+        //input.add('a');
+        //input.add('0');
+        //System.out.println(input.getValue());
+    }
+}
