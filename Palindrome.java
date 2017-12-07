3081b7c31f50890980484f34161825ba1fe6051d
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-07 15:38:00 -0600

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,11 @@
+package Palindrome;
+
+public class Palindrome {
+    public static boolean isPalindrome(String word) {
+        throw new UnsupportedOperationException("Waiting to be implemented.");
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(Palindrome.isPalindrome("Deleveled"));
+    }
+}
