e985385dab20c5423bd3bdad772b35dba986e1f1
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-07 20:36:18 -0600

    Added problem statement

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,3 +1,13 @@
+//  Created by Shawn O'Grady on 12/7/17.
+//  Copyright © 2017 Shawn O'Grady. All rights reserved.
+//
+/*
+ This code is a practice Java interview question from testdome.com
+ https://www.testdome.com/questions/java/binary-search-tree/12975?visibility=1&skillId=4
+ 
+ Problem statement: Write a function that checks if a given binary search tree contains a given value.
+ 	-Binary search tree (BST) is a binary tree where the value of each node is larger or equal to the values in all the nodes in that node's left subtree and is smaller than the values in all the nodes in that node's right subtree.
+ */
 package BinarySearchTree;
 
 class Node {
